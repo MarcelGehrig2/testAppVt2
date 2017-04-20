781759d0c4be1bcf231ab59a637272de9a214a80
MarcelGehrig <marcel.gehrig@ntb.ch>
2017-04-20 20:05:17 +0200

    beginner_tutorials package created. 'http://wiki.ros.org/ROS/Tutorials/catkin/CreatingPackage'

--- catkin_ws/build/catkin_generated/setup_cached.sh
+++ catkin_ws/build/catkin_generated/setup_cached.sh
@@ -7,7 +7,4 @@
 # new environment variables
 
 # modified environment variables
-export CMAKE_PREFIX_PATH="/home/mgehrig2/VT2/Software/testAppVt2/catkin_ws/devel:$CMAKE_PREFIX_PATH"
 export PWD="/home/mgehrig2/VT2/Software/testAppVt2/catkin_ws/build"
-export ROSLISP_PACKAGE_DIRECTORIES="/home/mgehrig2/VT2/Software/testAppVt2/catkin_ws/devel/share/common-lisp"
-export ROS_PACKAGE_PATH="/home/mgehrig2/VT2/Software/testAppVt2/catkin_ws/src:$ROS_PACKAGE_PATH"
--- catkin_ws/build/cmake_install.cmake
+++ catkin_ws/build/cmake_install.cmake
@@ -117,6 +117,7 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
   include("/home/mgehrig2/VT2/Software/testAppVt2/catkin_ws/build/gtest/cmake_install.cmake")
+  include("/home/mgehrig2/VT2/Software/testAppVt2/catkin_ws/build/beginner_tutorials/cmake_install.cmake")
 
 endif()
 
