be16450f6e91cc2c654380bed816c7b39b52f033
MarcelGehrig <marcel.gehrig@ntb.ch>
2017-05-15 13:53:27 +0200

    simpleROSNode works.
    1.) shows as ROS node '/simpleROSNode'
    2.) sends "Hello World" to 'rqt_console__Console'

--- simpleROSNode/main.cpp
+++ simpleROSNode/main.cpp
@@ -1,9 +1,30 @@
 #include <iostream>
 
+//ROS stuff
+#include <ros/ros.h>
+#include <ros/console.h>
+//#include "std_msgs/String.h"
+
+
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-	cout << "Hello World!" << endl;
+	cout << "TEST: 'simpleROSNode' stated" << endl;
+
+
+	ros::init(argc, argv, "simpleROSNode");
+	ros::NodeHandle n;
+	ROS_INFO("%s", "Hello World");
+	ROS_DEBUG("Hello %s", "World");
+
+
+	cout << "ROS node shuts down after 10 seconds." << endl;
+	sleep(10);
+
+
+
+
+	cout << "TEST: 'simpleROSNode' finished" << endl;
 	return 0;
 }
